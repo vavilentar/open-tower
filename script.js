3a6a1d116c2c7ef1fcb154c48945d23b4f385a88
Roman Pchelkin <epos.91@mail.ru>
2022-08-17 12:59:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const data = document.querySelectorAll('.text-input');
 const btn = document.querySelector('.btn-sb');
 
 
-btn.addEventListener('click', (e) => {
-	e.preventDefault();
+btn.addEventListener('click', () => {
 	console.log(getData())
 })
 
 function getData() {
+
 	let dataObj = {
 		name: data[0].value,
 		phone: data[1].value,
 		mail: data[2].value,
 	}
-	resetForms();
 	return JSON.stringify(dataObj);
 }
 
-function resetForms () {
-	for (let i = 0; i < data.length; i++) {
-		data[i].value = '';
-	}
-	document.querySelector('.checkbox').checked = false;
-}
+// function resetForms () {
+// 	for (let i = 0; i < data.length; i++) {
+// 		data[i].value = '';
+// 	}
+// 	document.querySelector('.checkbox').checked = false;
+// }
