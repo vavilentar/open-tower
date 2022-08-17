91e04a6168358e1cf13f791904ad8e8dbc513879
Roman Pchelkin <epos.91@mail.ru>
2022-08-17 14:57:30 +0300

    fix

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const data = document.querySelectorAll('.text-input');
 const btn = document.querySelector('.btn-sb');
+const form = document.querySelector('.wrapper');
+const modal = document.querySelector('.success-block');
 
-
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
 	console.log(getData())
+	e.preventDefault();
 })
 
 function getData() {
-
-	let dataObj = {
-		name: data[0].value,
-		phone: data[1].value,
-		mail: data[2].value,
+	
+	if (data[0].value != '' && data[1] != '' && data[2] != '' && document.querySelector('.checkbox').checked == true) {
+		let dataObj = {
+			name: data[0].value,
+			phone: data[1].value,
+			mail: data[2].value,
+		}
+	
+		form.classList.remove('wrapper-show');
+		modal.classList.add('success-show');
+	
+		return JSON.stringify(dataObj);
+	} else {
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].value == '') {
+				data[i].style = 'border-bottom: 2px solid red';
+			}
+		}
 	}
-	return JSON.stringify(dataObj);
 }
 
 // function resetForms () {
