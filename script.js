b6556795a731d3646bbf3dea5deded286bade5d3
Roman Pchelkin <epos.91@mail.ru>
2022-08-17 12:46:07 +0300

    json

---
+++ script.js
@@ -0,0 +1,25 @@
+const data = document.querySelectorAll('.text-input');
+const btn = document.querySelector('.btn-sb');
+
+
+btn.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(getData())
+})
+
+function getData() {
+	let dataObj = {
+		name: data[0].value,
+		phone: data[1].value,
+		mail: data[2].value,
+	}
+	resetForms();
+	return JSON.stringify(dataObj);
+}
+
+function resetForms () {
+	for (let i = 0; i < data.length; i++) {
+		data[i].value = '';
+	}
+	document.querySelector('.checkbox').checked = false;
+}
